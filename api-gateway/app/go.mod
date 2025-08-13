84526bb42d6df9e4413a4ed55dcb6b449eca3a1e
Eritsian David <wruser123@gmail.com>
2025-08-13 22:46:09 +0300

    api-gateway: NewLogger() added

--- api-gateway/app/go.mod
+++ api-gateway/app/go.mod
@@ -18,7 +18,6 @@ require (
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
-	github.com/jackc/puddle/v2 v2.2.2 // indirect
 	github.com/labstack/gommon v0.4.2 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
@@ -35,7 +34,6 @@ require (
 	go.uber.org/multierr v1.10.0 // indirect
 	golang.org/x/crypto v0.37.0 // indirect
 	golang.org/x/net v0.35.0 // indirect
-	golang.org/x/sync v0.13.0 // indirect
 	golang.org/x/sys v0.32.0 // indirect
 	golang.org/x/text v0.24.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20250218202821-56aae31c358a // indirect
--- api-gateway/app/internal/logger/logger.go
+++ api-gateway/app/internal/logger/logger.go
@@ -1,9 +1,11 @@
 package logger
 
 import (
+	"os"
 	"sync"
 
 	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 )
 
 var (
@@ -39,4 +41,27 @@ func Fatal(msg string, fields ...zap.Field) {
 
 func Panic(msg string, fields ...zap.Field) {
 	Log.Panic(msg, fields...)
+}
+
+func NewLogger(env string) *zap.Logger {
+	var cfg zapcore.EncoderConfig
+	var encoder zapcore.Encoder
+
+	cfg = zap.NewDevelopmentEncoderConfig()
+	cfg.EncodeTime = zapcore.TimeEncoderOfLayout("15:04:05")
+
+	if env == "dev" {
+		cfg.EncodeLevel = zapcore.CapitalColorLevelEncoder
+		encoder = zapcore.NewConsoleEncoder(cfg)
+	} else {
+		encoder = zapcore.NewJSONEncoder(cfg)
+	}
+
+	core := zapcore.NewCore(
+		encoder,
+		zapcore.AddSync(os.Stdout),
+		zapcore.DebugLevel,
+	)
+
+	return zap.New(core, zap.AddCaller(), zap.AddStacktrace(zapcore.ErrorLevel))
 }
